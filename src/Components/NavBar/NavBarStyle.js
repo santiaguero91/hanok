0e02475478fd3ace6ddb5db420076e5e6bc1fa12
Santiago Nicolás Agüero Urquiza <santiagonaguero@gmail.com>
2023-09-19 19:14:04 -0300

    sticky bar

--- src/Components/NavBar/NavBarStyle.js
+++ src/Components/NavBar/NavBarStyle.js
@@ -3,12 +3,14 @@ import { Box, Typography, TextField, Button, FormLabel } from "@mui/material";
 
 export const MainDiv = styled(Box)`
   width: 100%;
+  position: sticky;
+  top: 0px;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
   background-color: rgba(191, 191, 191);
   color: black;
   align-items: center;
-  padding: 0.5% 0 0.5% 0;
+  padding: 1% 0 1% 0;
 `;
 
